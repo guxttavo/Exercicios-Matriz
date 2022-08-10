7bf34bbc289345a818d57848aa8643888c91adcd
GusttavoBarbosa <gustavowbarbosas@gmail.com>
2022-08-10 18:25:44 -0300

    Initial commit

---
+++ src/Ex01.java
@@ -0,0 +1,7 @@
+/*
+    Faça um programa que peça uma nota, entre zero e dez. Mostre uma mensagem caso o valor seja inválido
+    e continue a pedir até que o usuário informe um valor válido.
+    */
+
+public class Ex01 {
+}
