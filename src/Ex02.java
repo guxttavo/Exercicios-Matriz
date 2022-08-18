00b58ac845c09789d4659b9ebb4542141bf4ad7b
GusttavoBarbosa <gustavowbarbosas@gmail.com>
2022-08-17 23:26:27 -0300

    exercicio 02

--- src/Ex02.java
+++ src/Ex02.java
@@ -14,6 +14,35 @@ public class Ex02 {
         Locale.setDefault(Locale.US);
         Scanner sc = new Scanner(System.in);
 
+        System.out.println("Qual a ordem da matriz? ");
+        int ordem = sc.nextInt();
 
+        double[][] matriz = new double[ordem][ordem];
+
+
+        System.out.println("Digite os valores da matriz: ");
+        for (int i = 0; i < matriz.length; i++) {
+            for (int j = 0; j < matriz.length; j++) {
+                matriz[i][j] = sc.nextDouble();
+            }
+        }
+
+        double soma = 0.0;
+        for (int i = 0; i < matriz.length; i++) {
+            for (int j = 0; j < matriz.length; j++) {
+                if (i == j) {
+                    soma = soma + matriz[i][j];
+
+                }
+            }
+        }
+
+        System.out.println("Matriz digitada: ");
+        for (int i = 0; i < matriz.length; i++) {
+            for (int j = 0; j < matriz.length; j++) {
+                System.out.print(matriz[i][j] + " ");
+            }
+            System.out.println();
+        }
     }
 }
