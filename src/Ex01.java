d43d1d625768eb3f2557989994af67c224d38396
GusttavoBarbosa <gustavowbarbosas@gmail.com>
2022-08-17 22:58:48 -0300

    exercicio 01

--- src/Ex01.java
+++ src/Ex01.java
@@ -1,7 +1,49 @@
 /*
-    Faça um programa que peça uma nota, entre zero e dez. Mostre uma mensagem caso o valor seja inválido
-    e continue a pedir até que o usuário informe um valor válido.
-    */
+
+-->Gere e imprime uma matriz M 4x4 com valores aleatórios entre 0-9.
+-->Após isso, determine o maior número da matriz e a sua posição(linha, coluna).
+
+*/
+
+import java.util.Random;
 
 public class Ex01 {
+    public static void main(String[] args) {
+
+        int[][] numerosAleatorios = new int[4][4];
+
+        Random numeroRandom = new Random();
+
+        for (int i = 0; i < numerosAleatorios.length; i++) {
+            for (int j = 0; j < numerosAleatorios[i].length; j++) {
+                numerosAleatorios[i][j] = numeroRandom.nextInt(100);
+            }
+        }
+
+        int maior = 0;
+        int linha = 0;
+        int coluna = 0;
+        for (int i = 0; i < numerosAleatorios.length; i++) {
+            for (int j = 0; j < numerosAleatorios.length; j++) {
+                if (numerosAleatorios[i][j] > maior) {
+                    maior = numerosAleatorios[i][j];
+                    linha = i;
+                    coluna = j;
+                }
+            }
+        }
+
+        for (int i = 0; i < numerosAleatorios.length; i++) {
+            for (int j = 0; j < numerosAleatorios[i].length; j++) {
+                System.out.print(numerosAleatorios[i][j] + " ");
+            }
+            System.out.println();
+        }
+
+        System.out.println("Maior valor = " + maior);
+        System.out.println("Linha = " + linha);
+        System.out.println("Coluna = " + coluna);
+
+
+    }
 }
---
+++ src/Ex02.java
@@ -0,0 +1,19 @@
+/*
+-->Ler um número inteiro N representando a ordem de uma matriz quadrada.
+-->Ler uma matriz de números reais, depois mostrar a soma da diagonal principal da matriz.
+
+*/
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Ex02 {
+
+    public static void main(String[] args) {
+
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+
+    }
+}
